760834420a5ddc47674cd9b39b7abb7a8f46355d
Sanjay Singh <sanjay.singh@iscits.in>
2026-09-09 17:37:30 +0530

    Loan Finalization update

---
+++ src/app/payroll/loan-finalization/page.tsx
@@ -0,0 +1,5 @@
+import LoanFinalizationPage from "@/features/payroll/components/LoanFinalizationPage";
+
+export default function PayrollLoanFinalizationRoute() {
+  return <LoanFinalizationPage />;
+}
